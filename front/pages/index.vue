fb66bfdd9fd7c6f417ee2db529abaa8bd2f6b3df
kankan-akan <yuh96_favobb@yahoo.co.jp>
2021-08-21 00:40:08 +0900

    Merge subtree

---
+++ front/pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="container">
+  <v-btn @click="getApi( )">GET API</v-btn>
+  <div>{{ message }}</div>
+</div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      message: ''
+    }
+  },
+  methods: {
+    getApi() {
+      const url = "/api/v1/hello"
+      this.$axios.get(url)
+        .then((res) => {
+          this.message = res.data
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
+  }
+}
+</script>
