e1fe27d8e21b1abe4ff746980e472840c5d7a0d2
kankan-akan <yuh96_favobb@yahoo.co.jp>
2021-08-25 00:39:01 +0900

    install vuetify

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -37,13 +37,15 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
+    '@nuxtjs/vuetify'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
-    '@nuxtjs/proxy'
+    '@nuxtjs/proxy',
+    
   ],
 
   proxy: {
--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,8 +1,72 @@
 <template>
-<div class="container">
-  <v-btn @click="getApi( )">GET API</v-btn>
-  <div>{{ message }}</div>
-</div>
+<body>
+  <header>
+    <div class ="container">
+      <div class ="header-logo">LOGO</div>
+      <div class ="header-left">
+        <a class = "searchdetail" href="">詳細検索</a>
+        <a class = "searchmap" href="">地図から探す</a>
+      </div>
+      <a class ="login" href="">ログイン</a>  
+    </div>
+
+        <v-btn @click="getApi( )">GET API</v-btn>
+  　　<div>{{ message }}</div>
+
+  </header>
+  <div class ="main">
+    <div class ="container">
+      <div class ="contents">
+        <p1>しょしゃぐりーんくらぶ</p1>
+        <p>書写グリーン倶楽部</p>
+          <table>
+            <tr>
+              <td class ="empty"></td class ="empty"> <th>平日</th>　<th>土日</th>
+            </tr>
+            <tr>
+              <th>ボール単価</th>　<td>10円/1球</td>　<td>12円/1球</td>
+            </tr>
+          </table>
+      </div>
+      <div class ="contents">
+        <p1>さいがごるふれんしゅうじょう</p1>
+        <p>才加ゴルフ練習場</p>
+          <table>
+            <tr>
+              <td class ="empty"></td class ="empty"> <th>平日</th>　<th>土日</th>
+            </tr>
+            <tr>
+              <th>ボール単価</th>　<td>10円/1球</td>　<td>12円/1球</td>
+            </tr>
+          </table>
+      </div>
+      <div class ="contents">
+        <p1>にっけごるふくらぶきょうぐちせんたー</p1>
+        <p>ニッケゴルフ倶楽部京口センター</p>
+          <table>
+            <tr>
+              <td class ="empty"></td> <th>平日</th> <th>土日</th>
+            </tr>
+            <tr>
+              <th>ボール単価</th> <td>10円/1球</td> <td>12円/1球</td>
+            </tr>
+          </table>
+      </div>
+      <div class ="contents">
+        <p1>あおやまごるふれんしゅうじょう</p1>
+        <p>青山ゴルフ練習場</p>
+          <table>
+            <tr>
+              <td class ="empty"> </td class ="empty"><th>平日</th> <th>土日</th>
+            </tr>
+            <tr>
+              <th>ボール単価</th> <td>10円/1球</td><td>12円/1球</td>
+            </tr>
+          </table>
+      </div>
+    </div>
+  </div>
+</body>
 </template>
 
 <script>
