76595572e104e15570cc3bbfbd54b1cf0e45a82f
kankan-akan <yuh96_favobb@yahoo.co.jp>
2021-09-06 00:23:21 +0900

    output placename

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -14,27 +14,35 @@
       </v-row> 
     </v-container>
   </v-app-bar>
-  <v-main >
-    <v-container >
+
+  <v-main>
+    <v-container>
       <v-row>
-      <v-col 
-        
-        xs = "12"
-        sm = "3"
-      >
+        <v-col 
+          v-for= "(place, i) in places"
+          :key = " i "
+          cols = "12"
+          xs = "12"
+          sm = "3"
+        >
         <v-card 
           justify = "space-around"
         >
-          <v-card-text >
-            <div v-text = "message"></div>
+          <v-card-text class="my-0 pa-2 black--text">
+            <div v-text= "place.ruby"></div>
+            <div 
+              class = "text-h6"
+              v-text = "place.place"
+            >
+            </div>
           </v-card-text>
             <v-divider class="my-2"></v-divider>
               <table>
                 <tr>
-                  <td class ="empty"></td class ="empty"> <th>平日</th>　<th>土日</th>
+                  <td class ="empty"></td> <th>平日</th><th>土日</th>
                 </tr>
                 <tr>
-                  <th>ボール単価</th>　<td>10円/1球</td>　<td>12円/1球</td>
+                  <th>ボール単価</th> <td>10円/1球</td><td>12円/1球</td>
                 </tr>
               </table>
           
@@ -46,12 +54,11 @@
                 @click="overlay = !overlay"
               >
                 詳細
-                <br>
+                <br/>
                   <v-icon>mdi-chevron-double-down</v-icon>
-                </br>
               </v-btn>
           </v-card-actions>
-          <div id = "app-1" @click ="overlay = false">
+          <div @click ="overlay = false">
           <v-overlay :value="overlay" >
             <v-container>
               <v-row >
@@ -61,8 +68,12 @@
                   width ="400"
                 >
                   <v-card-text class ="black--text"> 
-                    <div>furigana</div>
-                    <div class ="text-h6">Hello world</div>
+                    <div v-text= "place.ruby"></div>
+                    <div 
+                      class ="text-h6"
+                      v-text= "place.place"
+                      >
+                    </div>
                     <v-divider class ="grey lighten-2 my-2"></v-divider>
                   </v-card-text>
                   <v-card-actions class ="justify-center">
@@ -81,7 +92,7 @@
           </div>
         </v-card>
 
-      </v-col>
+        </v-col>
       </v-row>
       
     </v-container>
@@ -90,24 +101,20 @@
 </template>
 
 <script>
- export default {
-  name: ' ',
-  data: () => ({
-    message: 'hello'
-    
-  }),
-}
-
-</script>
-
-
-<script>
 export default {
-  name: 'app-1',
   data: () => ({
+      // modalwindow 
     absolute: true,
     overlay: false,
-    
+      // output 
+    places: [
+      {ruby: 'しょしゃぐりーんくらぶ', place: '書写グリーン倶楽部'  },
+      {ruby: 'さいがごるふれんしゅうじょう', place: '才加ゴルフ練習場' },
+      {ruby: 'あおやまごるふれんしゅうじょう', place: '青山ゴルフ練習場' },
+      {ruby: 'にっけごるふくらぶきょうぐち', place: 'ニッケゴルフ倶楽部京口' }
+
+    ]
+
   }),
 }
 </script>
