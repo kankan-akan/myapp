3f40d97210cef5e77607b76ff9a92d0e3142bbe3
kankan-akan <yuh96_favobb@yahoo.co.jp>
2021-10-17 01:29:29 +0900

    set user menus

--- front/pages/user/login.vue
+++ front/pages/user/login.vue
@@ -1,4 +1,5 @@
 <template>
+  <v-app>
     <v-app-bar 
       app 
       height="100" 
@@ -15,16 +16,40 @@
           <h2>{{ $store.state.auth.loggedIn }}</h2>
           <p>{{ $auth.user }}</p>
           <v-spacer />
-            <v-btn>
-              username
-            </v-btn>
             <v-btn @click ="$auth.logout()">
-             LOGOUT
+             logout
             </v-btn>
+           <div>
+            <v-menu 
+              bottom
+              offset-y
+            >
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  color="primary"
+                  dark
+                  v-bind="attrs"
+                  v-on="on"
+                >
+                  @username
+                </v-btn>
+              </template>
+              <v-list>
+                <v-list-item
+                  v-for="(item, index) in items"
+                  :key="index"
+                  :to="item.link"
+                  link
+                >
+                  <v-list-item-title>{{ item.title }}</v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+          </div>
         </v-row>
       </v-container>
     </v-app-bar>
-
+  </v-app>
 </template>
 
 <script>
@@ -34,7 +59,12 @@
 
 export default {
   data: () => ({
-    
+    items: [
+        { title: 'プロフィール', link: '' },
+        { title: 'ユーザー設定', link: '' },
+        {title: 'ログアウト', link: '/user/logout'},
+
+      ],
   }),
 
   computed: {
@@ -44,7 +74,7 @@ export default {
   },
 
   methods: {
-
+    
   },
 }
 
---
+++ front/pages/user/logout.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-container class ="text-center">
+      <div >
+      ログアウトを実行します。
+      </div>
+      <v-btn @click ="$auth.logout()">
+        OK
+      </v-btn>
+      <v-btn to="/user/login">
+        キャンセル
+      </v-btn>
+    </v-container>
+
+  </v-app>
+</template>
+
+<script>
+
+export default {
+ 
+}
+</script>
