0e8c7dfe2bee977773064cf14361dfb35a2202e9
kankan-akan <yuh96_favobb@yahoo.co.jp>
2021-12-26 18:09:57 +0900

    ログインユーザー名表示

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   namespace :v1 do
+    namespace :auth do
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
         registrations: 'v1/auth/registrations'
       }
       
       # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+      get 'users', to: 'users#members_only'
+      get '/users/index', to: 'users#index'
+    end
   end
 end
