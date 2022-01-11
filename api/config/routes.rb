4bdec8bd5db5d5a90b96c2dffc09bd80be26463c
kankan-akan <yuh96_favobb@yahoo.co.jp>
2022-01-12 01:35:53 +0900

    ルーティング修正

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,22 +1,21 @@
 Rails.application.routes.draw do
   namespace :v1 do
+    mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+      registrations: 'v1/auth/registrations'
+    }
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     namespace :auth do
-      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
-        registrations: 'v1/auth/registrations'
-      }
-      # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
       get 'users', to: 'users#members_only'
       get '/users/index', to: 'users#index'
-
       resources :posts, only: [:index, :show, :create, :destroy]
-
     end
+
+    mount_devise_token_auth_for 'AdminRange', at: 'range_auth', controllers: {
+      registrations: 'v1/range_auth/registrations'
+    }
+      # Define routes for AdminRange within this block.
     namespace :range_auth do
-      mount_devise_token_auth_for 'AdminRange', at: 'range_auth', controllers: {
-        registrations: 'v1/range_auth/registrations'
-      }
-        # Define routes for AdminRange within this block.
-    
+      
     end
   end
 end
