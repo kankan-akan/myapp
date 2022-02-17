20ff8405e227546e7dfd1fc7430b4c859c1be7ea
kankan-akan <yuh96_favobb@yahoo.co.jp>
2022-02-17 23:34:25 +0900

    マイページ/tab作成

--- front/pages/user/plofile.vue
+++ front/pages/user/plofile.vue
@@ -2,14 +2,73 @@
   <v-app>
     <Header />
     <v-main>
-      <v-card>
-        <v-card-title>
-          <div>plofile</div>
-        </v-card-title>
-      
+      <v-card max-width="700" class="mx-auto">
+        <v-container>
+          <v-card-title>plofile</v-card-title>
+        
+          <div></div>
+          <v-tabs>
+            <v-tab>post</v-tab>
+            <v-tab>like</v-tab>
+            <v-tab>comment</v-tab>
+            <v-tab>reservation</v-tab>
+            <v-tab>bookmark</v-tab>
+          </v-tabs>
+          <v-item-group
+            v-model="selected"
+            multiple
+          >
+            <v-row dense>
+              <v-col
+                v-for="(item, i) in items"
+                :key="i"
+                cols="12"
+                sm="3"
+              >
+                <v-item v-slot="{ active, toggle }">
+                  <v-img
+                    :src="`https://cdn.vuetifyjs.com/images/${item.src}`"
+                    height="150"
+                    class="text-right pa-1"
+                    @click="toggle"
+                  >
+                    <v-btn
+                      icon
+                      dark
+                    >
+                      <v-icon>
+                        {{ active ? 'mdi-heart' : 'mdi-heart-outline' }}
+                      </v-icon>
+                    </v-btn>
+                  </v-img>
+                </v-item>
+              </v-col>
+            </v-row>
+          </v-item-group>
+        </v-container>
       </v-card>
     </v-main>
-    
-    
   </v-app>
 </template>
+
+<script>
+  export default {
+    data: () => ({
+      items: [
+        {
+          src: 'backgrounds/bg.jpg',
+        },
+        {
+          src: 'backgrounds/md.jpg',
+        },
+        {
+          src: 'backgrounds/bg-2.jpg',
+        },
+        {
+          src: 'backgrounds/md2.jpg',
+        },
+      ],
+      selected: [],
+    }),
+  }
+</script>
