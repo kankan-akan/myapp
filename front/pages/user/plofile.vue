28e3a6f2dba131f04d5719bb132d6cec246d8bf9
kankan-akan <yuh96_favobb@yahoo.co.jp>
2022-02-18 20:24:01 +0900

    mypage/avatar,name,userid

--- front/pages/user/plofile.vue
+++ front/pages/user/plofile.vue
@@ -2,49 +2,86 @@
   <v-app>
     <Header />
     <v-main>
-      <v-card max-width="700" class="mx-auto">
+      <v-card max-width="700" class="justify-center mx-auto">
         <v-container>
           <v-card-title>plofile</v-card-title>
-        
-          <div></div>
-          <v-tabs>
+          <v-row class="justify-center pa-3">
+            <v-avatar size="120">
+              <img
+                src="https://cdn.vuetifyjs.com/images/john.jpg"
+                alt="John"
+              >
+            </v-avatar>
+          </v-row>
+          <v-row class="justify-center">
+            <div>username</div>
+          </v-row>
+          <v-row class="justify-center">
+            <div>@userid</div>
+          </v-row>
+          <v-row>
+          <v-tabs v-model="tabs" fixed-tabs color="cyan">
             <v-tab>post</v-tab>
             <v-tab>like</v-tab>
             <v-tab>comment</v-tab>
             <v-tab>reservation</v-tab>
             <v-tab>bookmark</v-tab>
-          </v-tabs>
-          <v-item-group
-            v-model="selected"
-            multiple
-          >
-            <v-row dense>
-              <v-col
-                v-for="(item, i) in items"
-                :key="i"
-                cols="12"
-                sm="3"
-              >
-                <v-item v-slot="{ active, toggle }">
-                  <v-img
-                    :src="`https://cdn.vuetifyjs.com/images/${item.src}`"
-                    height="150"
-                    class="text-right pa-1"
-                    @click="toggle"
-                  >
-                    <v-btn
-                      icon
-                      dark
+
+            <v-tab-item>
+              <v-card flat>
+                <v-item-group
+                  v-model="selected"
+                  multiple
+                >
+                  <v-row dense>
+                    <v-col
+                      v-for="(item, i) in items"
+                      :key="i"
+                      cols="12"
+                      sm="3"
+                      xs="3"
                     >
-                      <v-icon>
-                        {{ active ? 'mdi-heart' : 'mdi-heart-outline' }}
-                      </v-icon>
-                    </v-btn>
-                  </v-img>
-                </v-item>
-              </v-col>
-            </v-row>
-          </v-item-group>
+                      <v-item v-slot="{ active, toggle }">
+                        <v-img
+                          :src="`https://cdn.vuetifyjs.com/images/${item.src}`"
+                          aspect-ratio="1"
+                          class="text-right pa-1"
+                          
+                        >
+                        <v-btn icon dark>
+                          <v-icon @click="toggle">
+                            {{ active ? 'mdi-heart' : 'mdi-heart-outline' }}
+                          </v-icon>
+                        </v-btn>
+                        </v-img>
+                      </v-item>
+                    </v-col>
+                  </v-row>
+                </v-item-group>
+              </v-card>
+            </v-tab-item>
+            <v-tab-item>
+              <v-card>
+
+              </v-card>
+            </v-tab-item>
+            <v-tab-item>
+              <v-card>
+
+              </v-card>
+            </v-tab-item>
+            <v-tab-item>
+              <v-card>
+
+              </v-card>
+            </v-tab-item>
+            <v-tab-item>
+              <v-card>
+
+              </v-card>
+            </v-tab-item>
+          </v-tabs>
+          </v-row>
         </v-container>
       </v-card>
     </v-main>
