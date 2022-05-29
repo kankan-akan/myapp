ae5228e3f1791cd51ee77d11e37fb2ad7482ed6d
kankan-akan <yuh96_favobb@yahoo.co.jp>
2022-05-29 20:44:16 +0900

    依存テスト追加

--- api/spec/requests/v1/auth/users_spec.rb
+++ api/spec/requests/v1/auth/users_spec.rb
@@ -3,8 +3,13 @@ require 'rails_helper'
 RSpec.describe "Users", type: :request do
   describe "Test User" do
     before do
-      user = create(:user)
-      login user
+      @user = create(:user)
+      login @user
+    end
+
+    it "userを削除すると、userが書いたpostも削除する" do 
+      @user.posts.create(content: 'example text')
+      expect{ @user.destroy }.to change{ Post.count }.by(-1)
     end
 
     it "members_only" do
