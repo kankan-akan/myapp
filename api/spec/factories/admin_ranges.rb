477764a7c283ee473c55518b2d075adb918abccc
kankan-akan <yuh96_favobb@yahoo.co.jp>
2022-06-09 01:11:08 +0900

    bundle install

--- api/spec/factories/admin_ranges.rb
+++ api/spec/factories/admin_ranges.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :admin_range do
     provider { "email" }
-    sequence(:name) { |n| "Example練習場#{n}" }
+    sequence(:name) { |n| "Example#{n}" }
     sequence(:uid) { |n| "Test#{n}@example.com" }
     email { uid }
     password { "password." }
