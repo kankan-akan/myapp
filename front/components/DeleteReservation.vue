dd8557db1b98c66f3c571ac1360931b8af7d3d61
kankan-akan <yuh96_favobb@yahoo.co.jp>
2022-07-12 17:02:35 +0900

    dialog

--- front/components/DeleteReservation.vue
+++ front/components/DeleteReservation.vue
@@ -1,13 +1,52 @@
 <template>
   <div>
-    <v-btn
-      rounded
-      outlined
-      color="green white--text lighten-1"
-      @click="deleteReservation(reservation.id)"
+    <v-dialog
+      v-model="dialog"
+      persistent
+      max-width="370"
     >
-      予約キャンセル
-    </v-btn>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          rounded
+          outlined
+          color="green white--text lighten-1"
+          @click="dialog = true"
+          v-bind="attrs"
+          v-on="on"
+        >
+          予約キャンセル
+        </v-btn>
+      </template>
+      <v-card>
+        <v-card-title class="text-h5">
+          予約をキャンセルしますか？
+        </v-card-title>
+        <v-card-text>
+          <div class="text-h6">レッスン内容</div>
+          <div class="ml-3 mt-3">
+            <div class="text-subtitle-1">{{ reservation.lesson.title }}</div>
+            <div>{{ reservation.date }}</div>
+          </div>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="green darken-1"
+            text
+            @click="deleteReservation(reservation.id)"
+          >
+            予約キャンセル
+          </v-btn>
+          <v-btn
+            color="green darken-1"
+            text
+            @click="dialog = false"
+          >
+            閉じる
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
@@ -16,6 +55,10 @@ import { mapActions } from 'vuex';
 
 export default {
   props:['reservation'],
+
+  data:() => ({
+    dialog: false
+  }),
 
   methods: {
     ...mapActions ({
