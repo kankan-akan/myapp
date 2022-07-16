639672407b0681aa09ce040180c2d0c4a098c848
kankan-akan <yuh96_favobb@yahoo.co.jp>
2022-07-17 00:48:00 +0900

    fltered outline

--- front/pages/index.js
+++ front/pages/index.js
@@ -5,7 +5,7 @@ export default {
     return {
       dialog: false,
       panel: [],
-      city: [],
+      city: '',
       loading: false,
       items: [],
       cities: [ '相生市', '赤穂市', '明石市', '朝来市', '芦屋市', '尼崎市', '淡路市', '伊丹市', '市川町', '猪名川町', '稲美町', '小野市', '加古川市', '加西市', '加東市', '神河町', '香美町', '上郡町', '川西市','神戸市', '佐用町', '三田市', '宍粟市', '新温泉町', '洲本市', '太子町', '多可町', '高砂市', '宝塚市', 'たつの市', '丹波市', '丹波篠山市', '豊岡市', '西宮市', '西脇市', '播磨町', '姫路市', '福崎町', '三木市', '南あわじ市', '養父市' ],
@@ -18,22 +18,19 @@ export default {
       outline: (state) => state.outline
     }),
     filteredOutlines() {
-      if(this.city !== '') {
-      const outline = this.outline.filter((f) => {
-        return this.city.some(x => f["city"] == x)
-      })
-        return outline
-        // let city = this.city
-        // for(let i in city) {
-        //   if (f.city[i] === undefined || f.city[i] !== city[i])
-        //     return this.outline
-        // }
-        // return true
-        
-      }else{
+      if (!this.city) { 
         return this.outline
+      } else {
+        const outline = this.outline.filter((f) => {
+          return this.city.some(x => f["city"] == x)
+        })
+          return outline
       }
     }
+  },
+
+  watch: {
+    
   },
 
   created: async function() {
