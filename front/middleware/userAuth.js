7aa7bf42752c6a3b45a6c6e856425a16c4f63fe8
kankan-akan <yuh96_favobb@yahoo.co.jp>
2022-08-15 17:35:04 +0900

    middleware

--- front/middleware/userAuth.js
+++ front/middleware/userAuth.js
@@ -1,6 +1,6 @@
 export default function ({ store, redirect }) {
   // ユーザーが認証されていない場合
-  if (!store.state.rangeAuth.isLoggedIn) {
-    return redirect('/rangeLogin')
+  if (!store.state.auth.loggedIn) {
+    return redirect('/loginForm')
   }
 }
