7d538c5d642e8338a6c5d86c3407c7122a40701e
kankan-akan <yuh96_favobb@yahoo.co.jp>
2022-09-03 18:59:52 +0900

    console.log削除

--- front/middleware/rangeAuth.js
+++ front/middleware/rangeAuth.js
@@ -1,7 +1,6 @@
 export default function ({ store, redirect, route }) {
   // ユーザー認証がされていない場合
     if (!store.state.rangeAuth.isLoggedIn && route.path !== '/') {
-      console.log('middleware-rangeAuth')
       return redirect('/rangeLogin')
     }
 }
