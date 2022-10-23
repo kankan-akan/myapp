232fb856525719e92ce69dc0a8ed323ae029337d
kankan-akan <yuh96_favobb@yahoo.co.jp>
2022-10-23 15:21:09 +0900

    tab表示

--- front/components/DetailIndex.vue
+++ front/components/DetailIndex.vue
@@ -25,83 +25,101 @@
 
           <v-divider class="grey lighten-2 my-2"></v-divider>
 
-          <v-row no-gutter justify="center">
-            <v-col cols="10">
-              <v-img
-                v-if="place.image && place.image.url"
-                class="grey lighten-2"
-                contain
-                aspect-ratio="1.7"
-                :src="place.image.url"
-              ></v-img>
-            <v-sheet
-              v-else
-              class="d-flex justify-center align-center"
-              color="grey lighten-2"
-              height="250"
-              aspect-ratio="1.7"
-            >
-              <v-icon size="80">{{ 'mdi-image' }}</v-icon>
-            </v-sheet>
-            </v-col>
-          </v-row>
-          <!-- <table class="table-price">
-            <tr>
-              <td class="empty-price"></td> <th class="th-price">平日</th><th class="th-price">土日</th>
-            </tr>
-            <tr>
-              <th class="th-price">ボール単価</th> <td class="td-price">10円/1球</td><td class="td-price">12円/1球</td>
-            </tr>
-          </table> -->
-          <v-row dense justify="center">
-            <v-col cols="11">
-              <Equipment :equipment="place.equipment" />
-            </v-col>
-          </v-row>
-          <v-card outlined shaped>
-            <v-col class="kaigyo text--primary">{{ place.features }}</v-col>
-          </v-card>
-          <v-col class="d-flex align-center">
-            <v-icon>{{ 'mdi-golf-tee' }}</v-icon>
-            <div class="text-subtitle-1 ml-2 mr-15">打席数</div>
-            <v-divider vertical class="mr-3 grey lighten-1"></v-divider>
-            <span>{{ place.booths }}打席</span>
-          </v-col>
-          <v-col class="d-flex align-center">
-            <v-icon>{{ 'mdi-phone' }}</v-icon>
-            <div class="text-subtitle-1 ml-2 mr-11">電話番号</div>
-            <v-divider vertical class="mr-3 grey lighten-1"></v-divider>
-            <span>{{ place.phone_number }}</span>
-          </v-col>
-          <v-col class="d-flex align-center">
-            <v-icon>{{ 'mdi-monitor' }}</v-icon>
-            <div class="text-subtitle-1 ml-2">ホームページ</div>
-            <v-divider vertical class="mx-3 grey lighten-1"></v-divider>
-            <span>{{ place.link }}</span>
-          </v-col>
-          <v-card-title 
-            v-if="place.lessons.length !== 0"
-            class="font-weight-bold"
-          >
-           レッスン一覧
-          </v-card-title>
-          <v-row dense>
-            <v-col 
-              v-for="(lesson, i) in place.lessons"
-              :key="i"
-              cols="12"
-            >
-              <v-card @click="showLesson(lesson.id)">
-                <v-card-title>{{ lesson.title }}</v-card-title>
-                <v-card-subtitle>{{ lesson.coach }}プロ</v-card-subtitle>
-                <v-card-text class="black--text">
-                  <div class="kaigyo">{{ lesson.content }}</div>
-                </v-card-text>
+          <v-tabs color="success">
+            <v-tab class="text-subtitle-1 font-weight-bold">概要</v-tab>
+            <v-tab class="text-subtitle-1 font-weight-bold">レッスン</v-tab>
+          
+            <v-tab-item>
+              <v-row no-gutter justify="center">
+                <v-col cols="10" class="mt-2">
+                  <v-img
+                    v-if="place.image && place.image.url"
+                    class="grey lighten-2"
+                    contain
+                    aspect-ratio="1.7"
+                    :src="place.image.url"
+                  ></v-img>
+                  <v-sheet
+                    v-else
+                    class="d-flex justify-center align-center"
+                    color="grey lighten-2"
+                    height="250"
+                    aspect-ratio="1.7"
+                  >
+                    <v-icon size="80">{{ 'mdi-image' }}</v-icon>
+                  </v-sheet>
+                </v-col>
+              </v-row>
+              <!-- <table class="table-price">
+                <tr>
+                  <td class="empty-price"></td> <th class="th-price">平日</th><th class="th-price">土日</th>
+                </tr>
+                <tr>
+                  <th class="th-price">ボール単価</th> <td class="td-price">10円/1球</td><td class="td-price">12円/1球</td>
+                </tr>
+              </table> -->
+              <v-row dense justify="center">
+                <v-col cols="11">
+                  <Equipment :equipment="place.equipment" />
+                </v-col>
+              </v-row>
+              <v-card outlined shaped>
+                <v-col class="kaigyo text--primary">{{ place.features }}</v-col>
               </v-card>
-            </v-col>
-          </v-row>
+              <v-col class="d-flex align-center">
+                <v-icon>{{ 'mdi-golf-tee' }}</v-icon>
+                <div class="text-subtitle-1 ml-2 mr-15">打席数</div>
+                <v-divider vertical class="mr-3 grey lighten-1"></v-divider>
+                <span>{{ place.booths }}打席</span>
+              </v-col>
+              <v-col class="d-flex align-center">
+                <v-icon>{{ 'mdi-phone' }}</v-icon>
+                <div class="text-subtitle-1 ml-2 mr-11">電話番号</div>
+                <v-divider vertical class="mr-3 grey lighten-1"></v-divider>
+                <span>{{ place.phone_number }}</span>
+              </v-col>
+              <v-col class="d-flex align-center">
+                <v-icon>{{ 'mdi-monitor' }}</v-icon>
+                <div class="text-subtitle-1 ml-2">ホームページ</div>
+                <v-divider vertical class="mx-3 grey lighten-1"></v-divider>
+                <span>{{ place.link }}</span>
+              </v-col>
+            </v-tab-item>
+
+            <v-tab-item>
+              <v-card-title 
+                class="font-weight-bold"
+              >
+              レッスン一覧
+              </v-card-title>
+              <v-row dense>
+                <template v-if="place.lessons.length !== 0">
+                  <v-col 
+                    v-for="(lesson, i) in place.lessons"
+                    :key="i"
+                    cols="12"
+                  >
+                    <v-card @click="showLesson(lesson.id)">
+                      <v-card-title>{{ lesson.title }}</v-card-title>
+                      <v-card-subtitle>{{ lesson.coach }}プロ</v-card-subtitle>
+                      <v-card-text class="black--text">
+                        <div class="kaigyo">{{ lesson.content }}</div>
+                      </v-card-text>
+                    </v-card>
+                  </v-col>
+                </template>
+                <template v-else>
+                  <v-col class="d-flex justify-center">
+                    <div class="text-h6 font-weight-bold grey--text lighten-1">現在開講されているレッスンはありません</div>
+                  </v-col>
+                </template>
+              </v-row>
+            </v-tab-item>
+          </v-tabs>
         </v-card-text>
+
         <v-divider class="grey lighten-2"></v-divider>
+
         <v-card-actions class="justify-center">
           <v-btn
             color="success"
@@ -113,6 +131,7 @@
             </div>
           </v-btn>
         </v-card-actions>
+
       </v-card>
     </v-dialog>
     </v-col>
