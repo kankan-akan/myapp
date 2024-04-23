93017950f642b070b849fa1c9b8f41439f7b17d1
kankan-akan <yuh96_favobb@yahoo.co.jp>
2024-04-24 00:41:43 +0900

    update credentials

--- api/config/initializers/carrierwave.rb
+++ api/config/initializers/carrierwave.rb
@@ -10,8 +10,9 @@ CarrierWave.configure do |config|
   if Rails.env.production?
     config.storage :fog
     config.fog_provider = 'fog/aws'
-    config.fog_directory  = 'myapp-s3'
+    config.fog_directory  = 'myapp-strage'
     # config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/myapp-s3'
+    # config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/myapp-strage'
     config.fog_public = false
     config.fog_credentials = {
       provider: 'AWS',
